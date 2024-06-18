057a36469a515e17239df1a84ee5524f92999584
Sebastian Choren <sebastian.choren@gmail.com>
2024-06-18 18:11:58 -0300

    use kind

---
+++ scripts/setup_kind_cluster.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+PROJECT_ROOT=$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")
+KUBECONFIG_FILE=$(pwd)/tracetest.kubeconfig
+ENV_FILE=$PROJECT_ROOT/cluster.env
+
+
+if [[ "$1" == "--reset" ]]; then
+  kind delete cluster --name tracetest
+fi
+
+if ! kind get clusters | grep -q tracetest; then
+  kind create cluster  --name tracetest --kubeconfig $KUBECONFIG_FILE
+else 
+  echo "Cluster already exists"
+fi
+
+cat <<EOF > $ENV_FILE
+export KUBECONFIG=$KUBECONFIG:$KUBECONFIG_FILE
+kubectl config use-context kind-tracetest
+EOF
+
+source $ENV_FILE
+
+# install cert manager
+helm repo add jetstack https://charts.jetstack.io --force-update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.15.0 \
+  --set crds.enabled=true
+
+helm install ttdeps $PROJECT_ROOT/charts/tracetest-dependencies -f $PROJECT_ROOT/charts/tracetest-dependencies/values-kind.yaml
+helm install tt $PROJECT_ROOT/charts/tracetest-onprem
