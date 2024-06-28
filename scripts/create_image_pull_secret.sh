8e612d4223b99a7b406d6558d0e6e7106fd1f688
Sebastian Choren <sebastian.choren@gmail.com>
2024-06-28 15:40:21 -0300

    allow reading github credentials from env

--- scripts/create_image_pull_secret.sh
+++ scripts/create_image_pull_secret.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
 # Prompt for GitHub username
-read -p $'\e[1;32m Enter your GitHub username:\e[0m ' GITHUB_USERNAME
+if [ -z "$GITHUB_USERNAME" ]; then
+  read -p $'\e[1;32m Enter your GitHub username:\e[0m ' GITHUB_USERNAME
+else 
+  printf "\e[1;32mreading GitHub username from env.\e[0m\n"
+fi
 
 # Prompt for GitHub token
 
 # Prompt for GitHub token requirements
-cat <<EOF
+if [ -z "$GITHUB_DOCKER_PAT" ]; then
+  cat <<EOF
 Please provide a GitHub personal access token with the following requirements:
 - The token should have the "read:packages" scope to access GitHub Container Registry.
 
-To create a personal access token go to https://github.com/settings/tokens
+To create a personal access token go to https://github.com/settings/tokens/new?scopes=read:packages
 
 For more information, see: https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry#authenticating-to-the-container-registry
 EOF
 
-read -s -p $'\e[1;32m Enter your GitHub personal access token:\e[0m ' GITHUB_TOKEN
-echo
+  read -s -p $'\e[1;32m Enter your GitHub personal access token:\e[0m ' GITHUB_DOCKER_PAT
+  echo
+else
+  printf "\e[1;32mreading GitHub personal access token from env.\e[0m\n"
+fi
 
 # Create the ImagePullSecret in Kubernetes
 kubectl create secret docker-registry ghcr-secret \
   --docker-server=ghcr.io \
   --docker-username=$GITHUB_USERNAME \
-  --docker-password=$GITHUB_TOKEN \
-  --docker-email=your-email@example.com
+  --docker-password=$GITHUB_DOCKER_PAT \
+  --docker-email=example@example.com
 
 # Check if the secret was created successfully
 if [ $? -eq 0 ]; then
