08bc621fe48d55f8e778512b4e97e72b73800b19
Sebastian Choren <sebastian.choren@gmail.com>
2024-10-17 12:45:21 -0300

    test repo name

--- scripts/update_helm_repos.sh
+++ scripts/update_helm_repos.sh
@@ -5,4 +5,4 @@ helm repo add traefik https://traefik.github.io/charts --force-update
 helm repo add ory https://k8s.ory.sh/helm/charts --force-update
 helm repo add jetstack https://charts.jetstack.io --force-update
 helm repo add nats https://nats-io.github.io/k8s/helm/charts/ --force-update
-helm repo add opentelemetry https://open-telemetry.github.io/opentelemetry-helm-charts --force-update
+helm repo add opentelemetry-collector https://open-telemetry.github.io/opentelemetry-helm-charts --force-update
